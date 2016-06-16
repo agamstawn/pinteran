e78b817e522128f584e434cd9d68526ff186d97c
Agam Setiawan <setiawanagam@gmail.com>
2016-06-16 11:10:46 +0700

    Revert "Revert "deploy staging""
    This reverts commit 412b3456292deb3709c04de3eea383400ae72987.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,11 @@
 class Comment < ActiveRecord::Base
-	belongs_to :photo
+	 before_create :default_status
+        belongs_to :article
+        belongs_to :user
+        validates :content, presence: true, length: { minimum: 10 }
+
+        def default_status
+            self.status = "not active"
+        end
+
 end
